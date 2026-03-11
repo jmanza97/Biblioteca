013d3dc34003a532bbb47065d7835227c764df55
José Manzanares <jose@baodevs.com>
2026-03-10 21:22:11 -0500

    Add login test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -33,4 +33,52 @@ class AuthTest extends TestCase
 
         $this->assertAuthenticatedAs($user);
     }
+
+    public function test_it_cannot_login_with_invalid_credentials()
+    {
+        // Preparacion
+        $user = User::factory()->create([
+            'password' => bcrypt('test123'),
+        ]);
+
+        // Ejecucion
+        $response = $this->post('/api/v1/login', [
+            'email' => $user->email,
+            'password' => 'wrongpassword',
+        ]);
+
+        // Verificacion
+        $response->assertStatus(422);
+        $this->assertGuest();
+    }
+
+    // public function test_it_can_logout()
+    // {
+    //     // Preparacion
+    //     $user = User::factory()->create();
+
+    //     // Ejecucion
+    //     $response = $this->actingAs($user)->post('/api/v1/logout');
+
+    //     // Verificacion
+    //     $response->assertStatus(200);
+    //     $this->assertGuest();
+    // }
+
+    // public function test_it_can_get_profile()
+    // {
+    //     // Preparacion
+    //     $user = User::factory()->create();
+
+    //     // Ejecucion
+    //     $response = $this->actingAs($user)->get('/api/v1/profile');
+
+    //     // Verificacion
+    //     $response->assertStatus(200);
+    //     $response->assertJson([
+    //         'id' => $user->id,
+    //         'name' => $user->name,
+    //         'email' => $user->email,
+    //     ]);
+    // }
 }
